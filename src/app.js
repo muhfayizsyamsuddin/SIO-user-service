2c9cdbd723261ac9cdbbc179f95d6da20f1bd6b4
muhfayizsyamsuddin <muhfayiz14@gmail.com>
2026-08-18 20:59:52 +0800

    refactor: configuration cors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://sioms.faizms.com",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
