ad9d64b5c170c4162ddc1bc4186521edf885a8b1
muhfayizsyamsuddin <muhfayiz14@gmail.com>
2026-08-18 21:03:52 +0800

    refactor: configuration cors

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(express.json());
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 }));
+app.use(express.json());
 
 const userRouter = require('./routers/user');
 const authRouter = require('./routers/auth');
